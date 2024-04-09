a842951e8c97159f15a442753e015add1662e36a
sajid2726 <sajidshaikh6777@gmail.com>
2024-04-09 16:16:23 +0530

    Added 301 feature

--- src/main/java/com/jwtauth/RestCont.java
+++ src/main/java/com/jwtauth/RestCont.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController 
 public class RestCont {
 	public static final String message = "Final message";
+	public static String msg301 = "msg301";
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome  to JWT";
